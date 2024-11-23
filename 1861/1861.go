269283ae9aec12931bbdf0fbb66a98d3faed70d0
Azat Khabibulin <spektrokalter@gmail.com>
2024-11-24 02:49:58 +0500

    1861: combine gravity with transpose

--- 1861/1861.go
+++ 1861/1861.go
@@ -4,39 +4,27 @@ func RotateTheBox(box [][]byte) [][]byte {
 	return rotateTheBox(box)
 }
 
-func rotateTheBox(box [][]byte) [][]byte {
-	gravity(box)
-	return transpose(box)
-}
-
-func gravity(x [][]byte) {
-	for row := range x {
-		for i := len(x[row])-1; i >= 0; i-- {
-			if x[row][i] != '.' {
-				continue
-			}
-
-			stone := i
-			for stone >= 0 && x[row][stone] == '.' {
-				stone--
-			}
-
-			if stone >= 0 && x[row][stone] == '#' {
-				x[row][stone], x[row][i] = '.', '#'
-			}
-		}
-	}
-}
-
-func transpose(x [][]byte) [][]byte {
+func rotateTheBox(x [][]byte) [][]byte {
 	y := make([][]byte, len(x[0]))
 	for i := range y {
 		y[i] = make([]byte, len(x))
 	}
 
 	for row := range x {
-		for col := range x[row] {
-			y[col][row] = x[len(x)-1-row][col]
+		for i := len(x[row])-1; i >= 0; i-- {
+			if x[row][i] == '.' {
+				stone := i
+				for stone >= 0 && x[row][stone] == '.' {
+					stone--
+				}
+
+				if stone >= 0 && x[row][stone] == '#' {
+					x[row][stone], x[row][i] = '.', '#'
+					y[stone][len(x)-1-row] = x[row][stone]
+				}
+			}
+
+			y[i][len(x)-1-row] = x[row][i]
 		}
 	}
 
