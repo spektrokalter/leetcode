dca0f3074573776af02e43f615d5fd1a2450d662
Azat Khabibulin <spektrokalter@gmail.com>
2024-11-26 11:43:04 +0500

    2924: remove redundant memory

--- 2924/2924.go
+++ 2924/2924.go
@@ -5,15 +5,15 @@ func FindChampion(n int, edges [][]int) int {
 }
 
 func findChampion(n int, edges [][]int) int {
-	parent := map[int][]int{}
+	parents := map[int]int{}
 	for _, e := range edges {
-		parent[e[1]] = append(parent[e[1]], e[0])
+		parents[e[1]]++
 	}
 
 	champ := 0
 	roots := 0
 	for i := range n {
-		if parent[i] == nil {
+		if parents[i] == 0 {
 			roots++
 			champ = i
 		}
