9d6d89634da92aae8ec1cb55f244110a7395a948
Azat Khabibulin <azkhabibulin@ozon.ru>
2025-03-25 04:55:12 +0500

    3169: make wronganswer2 pass

--- 3169/3169.c
+++ 3169/3169.c
@@ -48,14 +48,18 @@ countDays(int days, int **meetings, int nmeetings, int *nnmeetings)
 			max1 = MAX(max1, (*m)[1]);
 		} else {
 
-			// ((1, 10) m=(20, 30))
+			// ((1, 10) m=(20, 30)) or ((1, 30) (10, 20) m=(25, 35))
 			//
-			if ((*m)[1] > max1)
-				busy += (*m)[1] - (*m)[0] + 1;
+			if ((*m)[1] > max1) {
+				if ((*m)[0] > max1)
+					busy += (*m)[1] - (*m)[0] + 1;
+				else
+					busy += (*m)[1] - max1;
+			}
 
 			max1 = MAX(max1, (*m)[1]);
 		}
-//		printf("%d\n", busy);
+//		printf("(%d, %d), (%d, %d), %d, %d\n", (*m)[0], (*m)[1], prev0, prev1, max1, busy);
 
 		prev0 = (*m)[0];
 		prev1 = (*m)[1];
@@ -89,7 +93,7 @@ example1(void)
 	int **meetingsmem = mkarr(meetings, ARRAY_LEN(meetings), 2);
 
 	int available = countDays(10, meetingsmem, ARRAY_LEN(meetings), NULL);
-	printf("3169.c:/^example1/	%d\n", available); // 2
+	printf("3169.c:/^example1/		%d\n", available); // 2
 }
 
 void
@@ -99,7 +103,7 @@ example2(void)
 	int **meetingsmem = mkarr(meetings, ARRAY_LEN(meetings), 2);
 
 	int available = countDays(5, meetingsmem, ARRAY_LEN(meetings), NULL);
-	printf("3169.c:/^example2/	%d\n", available); // 1
+	printf("3169.c:/^example2/		%d\n", available); // 1
 }
 
 void
@@ -109,9 +113,18 @@ example3(void)
 	int **meetingsmem = mkarr(meetings, ARRAY_LEN(meetings), 2);
 
 	int available = countDays(6, meetingsmem, ARRAY_LEN(meetings), NULL);
-	printf("3169.c:/^example3/	%d\n", available); // 0
+	printf("3169.c:/^example3/		%d\n", available); // 0
 }
 
+// 4, 8 → 8-4+1 = 5
+// 7, 42 → 5 + 42-8 = 39
+// 9, 18 → 39
+// 9, 33 → 39
+// 18, 19 → 39
+// 22, 31 → problem
+// 23, 39
+// 30, 46
+//
 void
 wronganswer1(void)
 {
@@ -122,6 +135,26 @@ wronganswer1(void)
 	printf("3169.c:/^wronganswer1/	%d\n", available); // 7
 }
 
+// 2, 44 → 44-2+1 = 43
+// 3, 55 → 43+55-44 = 54
+// 13, 21 → 54
+// 14, 24 → 54
+// 16, 19 → 54
+// 16, 38 → 54
+// 20, 25 → 54
+// 37, 40 → 54
+// 54, 58 → 54+58-55 = 57 → problem
+//
+void
+wronganswer2(void)
+{
+	int meetings[][2] = {{37, 40}, {16, 19}, {20, 25}, {3, 55}, {16, 38}, {2, 44}, {54, 58}, {14, 24}, {13, 21}};
+	int **meetingsmem = mkarr(meetings, ARRAY_LEN(meetings), 2);
+
+	int available = countDays(65, meetingsmem, ARRAY_LEN(meetings), NULL);
+	printf("3169.c:/^wronganswer2/	%d\n", available); // 8
+}
+
 int
 main(void)
 {
@@ -129,6 +162,7 @@ main(void)
 	example2();
 	example3();
 	wronganswer1();
+	wronganswer2();
 
 	return 0;
 }
