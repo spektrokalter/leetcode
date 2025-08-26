711f61c9bdf0c4f6f20f8027644ed2f6bda3865f
Azat Khabibulin <azkhabibulin@ozon.ru>
2025-08-27 03:18:12 +0500

    3000: accepted

--- 3000/3000.c
+++ 3000/3000.c
@@ -7,26 +7,29 @@ int
 areaOfMaxDiagonal(int **dim, int ndim, int *nndim)
 {
 	long double maxdiagonal = 0;
-	int maxarea = 0;
+	int area = 0;
 
 	for (int **p = dim; p != dim+ndim; ++p) {
 		long double diagonal = sqrt((*p)[0]*(*p)[0] + (*p)[1]*(*p)[1]);
-		if (diagonal >= maxdiagonal) {
+		if (diagonal == maxdiagonal) {
+			area = MAX(area, (*p)[0]*(*p)[1]);
+		}
+		else if (diagonal > maxdiagonal) {
 			maxdiagonal = diagonal;
-			maxarea = MAX(maxarea, (*p)[0]*(*p)[1]);
+			area = (*p)[0]*(*p)[1];
 		}
 	}
 
-	return maxarea;
+	return area;
 }
 
 void
 example1(void)
 {
-	int **dim = (int*[]){(int[]){9, 3}, (int[]){8, 6}};
+	int *dim[] = (int*[]){(int[]){9, 3}, (int[]){8, 6}};
 
 	int nndim = 2;
-	int area = areaOfMaxDiagonal(dim, 2, &nndim);
+	int area = areaOfMaxDiagonal(dim, nelem(dim), &nndim);
 	print("example1\n");
 	print("%d\n", area); // 48
 }
@@ -34,12 +37,27 @@ example1(void)
 void
 example2(void)
 {
-	int **dim = (int*[]){(int[]){3, 4}, (int[]){4, 3}};
+	int *dim[] = (int*[]){(int[]){3, 4}, (int[]){4, 3}};
 
 	int nndim = 2;
-	int area = areaOfMaxDiagonal(dim, 2, &nndim);
+	int area = areaOfMaxDiagonal(dim, nelem(dim), &nndim);
 	print("example2\n");
 	print("%d\n", area); // 12
+}
+
+void
+wonganswer1(void)
+{
+	int *dim[] = (int*[]){
+		(int[]){6, 5}, (int[]){8, 6}, (int[]){2, 10},
+		(int[]){8, 1}, (int[]){9, 2}, (int[]){3, 5},
+		(int[]){3, 5},
+	};
+
+	int nndim = 2;
+	int area = areaOfMaxDiagonal(dim, nelem(dim), &nndim);
+	print("wonganswer1\n");
+	print("%d\n", area); // 20
 }
 
 void
@@ -47,6 +65,7 @@ main(void)
 {
 	example1();
 	example2();
+	wonganswer1();
 
 	exits(0);
 }
