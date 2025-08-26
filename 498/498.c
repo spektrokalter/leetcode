bb795d6622435d0d46061cd3c303b91a4975e72f
Azat Khabibulin <azkhabibulin@ozon.ru>
2025-08-26 14:12:48 +0500

    498: store direction in indices' sign

--- 498/498.c
+++ 498/498.c
@@ -11,32 +11,29 @@ findDiagonalOrder(int **mat, int nmat, const int *nnmat, int *nout)
 
 	int *p = out;
 
-	int i = 0, j = 0;
-	int dir = -1;
+	int i = -1, j = 1;
 
-	while (i >= 0 && i < nmat && j >= 0 && j < *nnmat) {
-		*(p++) = mat[i][j];
+	while (abs(i) >= 1 && abs(i) <= nmat && abs(j) >= 1 && abs(j) <= *nnmat) {
+		*(p++) = mat[abs(i)-1][abs(j)-1];
 
-		i += dir;
-		j += -dir;
+		++i;
+		++j;
 
-		if (j >= *nnmat) {
-			j = *nnmat-1;
-			i += 2;
-			dir = -dir;
+		if (abs(j) > *nnmat) {
+			j = -*nnmat;
+			i = -i + 2;
 		}
-		if (i < 0) {
-			i = 0;
-			dir = -dir;
+		if (abs(i) < 1) {
+			i = 1;
+			j = -j;
 		}
-		if (i >= nmat) {
-			i = nmat-1;
-			j += 2;
-			dir = -dir;
+		if (abs(i) > nmat) {
+			i = -nmat;
+			j = -j + 2;
 		}
-		if (j < 0) {
-			j = 0;
-			dir = -dir;
+		if (abs(j) < 1) {
+			j = 1;
+			i = -i;
 		}
 	}
 
@@ -104,10 +101,27 @@ example2(void)
 }
 
 void
+wronganswer1(void)
+{
+	const int nnmat = 3;
+	int mat[][nnmat] = {
+		{2, 5, 8},
+		{4, 0, -1},
+	};
+
+	int narr = 0;
+	int *arr = findDiagonalOrder(mkarr(mat, nelem(mat), nnmat), nelem(mat), &nnmat, &narr);
+
+	print("wronganswer1\n");
+	arrprint(arr, narr);
+}
+
+void
 main(void)
 {
 	example1();
 	example2();
+	wronganswer1();
 
 	exits(0);
 }
