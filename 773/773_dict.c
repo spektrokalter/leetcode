2c8828316ea51721308df027d5610404a163ac32
Azat Khabibulin <azkhabibulin@ozon.ru>
2025-08-27 04:01:55 +0500

    773, 2601: fix file references

--- 773/773_dict.c
+++ 773/773_dict.c
@@ -150,7 +150,7 @@ slidingPuzzle(int **board, int rows, int *cols)
 void
 example1(void)
 {
-	printf("773.c:/example1/\n");
+	printf("773_dict.c:/example1/\n");
 	int *arr[] = (int*[]){(int[]){1, 2, 3}, (int[]){4, 0, 5}};
 	int cols[] = {3, 3};
 
@@ -161,7 +161,7 @@ example1(void)
 void
 example2(void)
 {
-	printf("773.c:/example2/\n");
+	printf("773_dict.c:/example2/\n");
 	int *arr[] = (int*[]){(int[]){1, 2, 3}, (int[]){5, 4, 0}};
 	int cols[] = {3, 3};
 
@@ -172,7 +172,7 @@ example2(void)
 void
 example3(void)
 {
-	printf("773.c:/example3/\n");
+	printf("773_dict.c:/example3/\n");
 	int *arr[] = (int*[]){(int[]){4, 1, 2}, (int[]){5, 0, 3}};
 	int cols[] = {3, 3};
 
@@ -183,7 +183,7 @@ example3(void)
 void
 wronganswer1(void)
 {
-	printf("773.c:/wronganswer1/\n");
+	printf("773_dict.c:/wronganswer1/\n");
 	int *arr[] = (int*[]){(int[]){3, 2, 4}, (int[]){1, 5, 0}};
 	int cols[] = {3, 3};
 
